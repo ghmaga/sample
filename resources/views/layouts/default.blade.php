53c0fe96cb13139615945516702ac79dd6468847
WesleyLee <liyl.space@gmail.com>
2018-07-05 09:07:53 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,21 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>@yield('title', 'LiYLi') - LiYLi 我没法像个农民那样善良，只是麦子还在对着太阳愤怒的生长</title>
-</head>
-<body>
-    @yield('content')
-</body>
+    <head>
+        <title>@yield('title', 'LiYLi') - LiYLi 我没法像个农民那样善良，只是麦子还在对着太阳愤怒的生长</title>
+        <link rel="stylesheet" type="text/css" href="/css/app.css">
+    </head>
+    <body>
+        @include('layouts._header')
+
+
+        <div class="container">
+            <div class="col-md-offset-1 col-md-10">
+                @yield('content')
+                @include('layouts._footer')
+            </div>
+        </div>
+
+
+
+    </body>
 </html>
